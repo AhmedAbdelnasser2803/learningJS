ce2895f028ba863ff1ac481c013f1d47b036589e
Abdelnasser <ahmed.abdelnasser2803@gmail.com>
2026-01-12 13:32:13 +0000

    relationalAndEqualityOperators

---
+++ lessons/04-relationalAndComparisnOperator.js
@@ -0,0 +1,17 @@
+//relational operator 
+// > - more than 
+// < - less than 
+// >= more than or equal 
+// <= less than or equal 
+
+var res = 10>5
+console.log(res)
+
+
+//equalit operators 
+value = 1 
+//lose comparison 
+console.log(value=='1')
+//strict comparison 
+console.log(value==='1')
+

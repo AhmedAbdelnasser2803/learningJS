5dfde52fb370ccaef7a2ae69e59391255e3c1628
Abdelnasser <ahmed.abdelnasser2803@gmail.com>
2026-01-12 12:45:12 +0000

    array&Object

--- lessons/01- variable .js
+++ lessons/01- variable .js
@@ -5,3 +5,8 @@ console.log("hello world")
 var firstName="ahmed"
 let lastName="abdelnasser"
 console.log(firstName+lastName)
+
+//dataType
+var string = "ahmed"  //string 
+var number = 15 //number 
+var boolen = true // boolen 
---
+++ lessons/03-objectsAndArrays.js
@@ -0,0 +1,25 @@
+//objects
+customer = {
+    firstName: 'ahmed',
+    lastName: 'abdelnasser',
+    car : ['volvo' ,'BMW' , 'marcides']
+}
+
+customer.firstName= 'mohamed'           //overwrite
+customer['lastName']= 'saad'            //ovewrite by the second method
+
+console.log(customer.firstName +' '+customer.lastName)      //concatination
+console.log(`${customer['firstName']} ${customer['lastName']}`)   //interpolation & the second method to get the value of the object
+console.log(`${customer.car[0]}`)
+console.log(customer)
+
+
+//array 
+car = ['volvo' ,'BMW' , 'marcides'] //define 
+car[0]= 'fiate'                     //overWrite for the first element
+console.log(car[0])                 //print the first element 
+car[4]= 'none'                      //insert the element on the array 
+console.log(car[3])                 //print undefine element on the array
+
+
+

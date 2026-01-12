45dff0c2b8e94f3d00dd9363f4ee049bedad4c7a
Abdelnasser <ahmed.abdelnasser2803@gmail.com>
2026-01-12 13:52:30 +0000

    logicalOperators

---
+++ lessons/05-logicalOperators.js
@@ -0,0 +1,11 @@
+// logical "AND"
+console.log(true&&false)
+// logical "OR"
+ageMoreThan18 = true
+isUSCitizen = false
+
+var eligibilityForDiverLiscense = (ageMoreThan18 || isUSCitizen)
+console.log(eligibilityForDiverLiscense);
+
+// logical "NOT "
+console.log(!true)
